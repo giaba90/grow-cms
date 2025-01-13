2bd68ac812170fc600db9b4b31021544725fb82c
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-13 11:48:43 +0100

    Aggiunta funzione per recuperare un articolo tramite API e aggiornamento della pagina per visualizzare il titolo e il contenuto

--- src/app/dashboard/articles/[id]/page.tsx
+++ src/app/dashboard/articles/[id]/page.tsx
@@ -1,8 +1,22 @@
-export default function Page() {
+async function fetchArticle(id: string): Promise<PostData> {
+  const res = await fetch(`${process.env.BASE_URL_API}/articles/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch article");
+  }
+  return res.json();
+}
+
+export default async function ArticlePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const article = await fetchArticle(params.id);
+
   return (
     <div>
-      <h1>Single article</h1>
-      <p>Welcome to the Single article</p>
+      <h1>{article.title}</h1>
+      <p>{article.content}</p>
     </div>
   );
 }
