5d00fc83506b000eed1866ba8a11fbd5428efa1d
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-13 14:55:59 +0100

    Aggiornata la configurazione di Apollo Server in graphql.ts per migliorare la gestione delle API; aggiunta esportazione del gestore e disabilitazione del body parser di Next.js

--- src/app/api/graphql.ts
+++ src/app/api/graphql.ts
@@ -1,21 +1,25 @@
-// graphql.ts
+// /pages/api/graphql.js
 import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "@/app/graphql/typeDefs";
-import { resolvers } from "@/app/graphql/resolvers";
+import { resolvers } from "@/app/graphql/resolvers"; // Carica i resolver
 
+// Inizializzazione di Apollo Server con il tipo e i resolver
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+// Crea il gestore per l'API route
 const handler = server.createHandler({
-  path: "/api/graphql",
+  path: "/api/graphql", // La rotta API per il GraphQL
 });
 
+// Esportiamo il gestore come default per la rotta API
 export default handler;
 
+// Configurazione per evitare che Next.js elabori il body in modo predefinito (necessario per Apollo Server)
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: false, // Disabilita il body parser di Next.js
   },
 };
