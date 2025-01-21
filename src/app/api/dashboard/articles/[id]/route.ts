f78ee09ebd4bf11d332a969c209bd916b759bd3f
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-21 18:50:55 +0100

    Aggiungi supporto per la cancellazione degli articoli: implementa il metodo DELETE nell'API per rimuovere un articolo specifico, gestendo errori e risposte appropriate.

--- src/app/api/dashboard/articles/[id]/route.ts
+++ src/app/api/dashboard/articles/[id]/route.ts
@@ -3,6 +3,7 @@ import { serializePost } from "@/app/utils/utility";
 import prisma from "@/app/lib/prisma/client";
 import { NextResponse } from "next/server";
 
+// GET /api/dashboard/articles/[id]
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -24,6 +25,27 @@ export async function GET(
 
     const postSerialized = serializePost(post);
     return NextResponse.json(postSerialized);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+// DELETE /api/dashboard/articles/[id]
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+
+  if (!id) {
+    return NextResponse.json({ error: "Missing post ID" }, { status: 400 });
+  }
+
+  try {
+    await prisma.post.delete({ where: { id: parseInt(id, 10) } });
+    return NextResponse.json({ message: "Post deleted" });
   } catch (error) {
     return NextResponse.json(
       { error: "Internal Server Error" },
