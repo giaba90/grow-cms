16495d53a79489fdbaa59c782e68259c2fcf97b8
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-21 19:00:56 +0100

    Aggiungi supporto per l'aggiornamento degli articoli: implementa il metodo PUT nell'API per modificare un articolo esistente, gestendo errori e risposte appropriate.

--- src/app/api/dashboard/articles/[id]/route.ts
+++ src/app/api/dashboard/articles/[id]/route.ts
@@ -32,6 +32,53 @@ export async function GET(
     );
   }
 }
+
+// PUT /api/dashboard/articles/[id]
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+
+  if (!id) {
+    return NextResponse.json({ error: "Missing post ID" }, { status: 400 });
+  }
+
+  try {
+    const data = await request.json();
+    const { title, content, url, description, status, featured, author_id } =
+      data;
+
+    if (!title || !content) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const updatedPost = await prisma.post.update({
+      where: { id: parseInt(id, 10) },
+      data: {
+        title,
+        content,
+        url,
+        description,
+        status,
+        featured,
+        author_id,
+      },
+    });
+
+    const updatedPostSerialized = serializePost(updatedPost);
+    return NextResponse.json(updatedPostSerialized);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
 // DELETE /api/dashboard/articles/[id]
 export async function DELETE(
   request: Request,
