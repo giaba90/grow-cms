ecd32c11669e6a75b4b2272775531801ae69d1d2
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-21 18:30:33 +0100

    Refactor article creation logic: move POST handler from create route to articles route, enhancing code organization and reusability.

--- src/app/api/dashboard/articles/route.ts
+++ src/app/api/dashboard/articles/route.ts
@@ -1,14 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma/client";
+import { serializePost } from "@/app/utils/utility";
 
 export async function GET() {
   try {
     const articles = await prisma.post.findMany();
     return NextResponse.json(articles);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch articles" },
+      { status: 500 }
+    );
+  }
+}
+
+// POST /api/dashboard/articles
+export async function POST(req: Request) {
+  try {
+    const data: PostData = await req.json();
+    const { title, content, url, description, status, featured, author_id } =
+      data;
+
+    if (!title || !content || !author_id) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        url,
+        description,
+        status,
+        featured,
+        author_id,
+      },
+    });
+
+    const newPostSerialized = serializePost(newPost);
+    return NextResponse.json(newPostSerialized, {
+      status: 201,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
