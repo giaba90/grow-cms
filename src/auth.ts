2b73904a45221a17ab783beb5d1639f329c24d44
Gianluca Barranca <info@gianlucabarranca.it>
2025-02-11 18:58:22 +0100

    fix error about typescript into auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,74 +1,3 @@
-/* import NextAuth from "next-auth";
-
-import { ZodError } from "zod";
-import Credentials from "next-auth/providers/credentials";
-import { signInSchema } from "./app/lib/validation";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/app/prisma/client";
-import bcrypt from "bcryptjs";
-async function getUserFromDb(email: string) {
-  try {
-    const user = await prisma.users.findUnique({
-      where: { email },
-      select: { id: true, email: true, password: true },
-    });
-    return user;
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    return null;
-  }
-}
-
-export const { handlers, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials): Promise<User | null> => {
-        try {
-          let user = null;
-
-          const { email, password } = await signInSchema.parseAsync(
-            credentials
-          );
-
-          user = await getUserFromDb(email);
-          if (!user) {
-            console.log("User not found");
-            return null;
-          }
-
-          if (!user.password) {
-            console.log("Invalid password");
-            return null;
-          }
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (!passwordsMatch) {
-            console.log("Password mismatch");
-            return null;
-          }
-
-          return {
-            id: user.id,
-            email: user.email ?? "",
-            role: "ADMIN",
-          };
-        } catch (error) {
-          if (error instanceof ZodError) {
-            return null;
-          }
-          throw error;
-        }
-      },
-    }),
-  ],
-});
- */
 import NextAuth from "next-auth";
 import { ZodError } from "zod";
 import Credentials from "next-auth/providers/credentials";
@@ -77,17 +6,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/app/prisma/client";
 import bcrypt from "bcryptjs";
 
-// Definizione del tipo dell'utente
-interface UserInterface {
-  id: string;
-  email: string;
-  password?: string;
-  role?: string;
-  name?: string;
-}
-
 // Funzione per ottenere l'utente dal database
-async function getUserFromDb(email: string): Promise<UserInterface | null> {
+async function getUserFromDb(email: string) {
   try {
     const user = await prisma.users.findUnique({
       where: { email },
@@ -110,7 +30,7 @@ export const { handlers, auth } = NextAuth({
         email: {},
         password: {},
       },
-      authorize: async (credentials): Promise<UserInterface | null> => {
+      authorize: async (credentials): Promise<User | null> => {
         try {
           // Parsing dei dati in ingresso tramite Zod
           const { email, password } = await signInSchema.parseAsync(
@@ -145,7 +65,7 @@ export const { handlers, auth } = NextAuth({
     async session({ session, token }) {
       if (session.user) {
         // Aggiungi 'role' alla sessione
-        session.user.role = token.role;
+        session.user.role = token.role as string;
       }
       return session;
     },
