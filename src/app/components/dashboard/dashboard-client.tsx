67d801046ab5428cad5ae779c2a7187013d9daba
Gianluca Barranca <info@gianlucabarranca.it>
2025-02-11 19:03:43 +0100

    remove margin from main into dashboard

--- src/app/components/dashboard/dashboard-client.tsx
+++ src/app/components/dashboard/dashboard-client.tsx
@@ -4,26 +4,21 @@ import { useState } from "react";
 import { Header } from "@/app/components/dashboard/header";
 import { Sidebar } from "@/app/components/dashboard/sidebar";
 
-export function DashboardClient({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+export function DashboardClient({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <Header
-                onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)}
-                isSidebarOpen={isSidebarOpen}
-            />
-            <div className="flex">
-                <Sidebar isOpen={isSidebarOpen} />
-                <main className={`flex-1 p-6 transition-all duration-300 ${isSidebarOpen ? 'md:ml-64' : 'md:ml-20'
-                    }`}>
-                    {children}
-                </main>
-            </div>
-        </div>
-    );
-} 
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header
+        onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        isSidebarOpen={isSidebarOpen}
+      />
+      <div className="flex">
+        <Sidebar isOpen={isSidebarOpen} />
+        <main className={`flex-1 p-6 transition-all duration-300`}>
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
