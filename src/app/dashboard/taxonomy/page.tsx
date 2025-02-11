21e0e0a514c3071e4b58ad41942cf0bc8ae39294
Gianluca Barranca <info@gianlucabarranca.it>
2025-02-11 18:07:15 +0100

    create basic page

---
+++ src/app/dashboard/taxonomy/page.tsx
@@ -0,0 +1,10 @@
+// app/dashboard/taxonomy/page.tsx
+
+export default function TaxonomyPage() {
+  return (
+    <div>
+      <h1>Taxonomie</h1>
+      <p>Gestione delle categorie e tag</p>
+    </div>
+  );
+}
