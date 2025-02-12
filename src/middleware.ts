9606d10f2662d958fb76209e2d8b15f003125d37
Gianluca Barranca <info@gianlucabarranca.it>
2025-02-12 12:30:45 +0100

    modified:   src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,37 @@ import { authOptions } from "./auth.config";
 import { APP_PATHS } from "./app/lib/constants/paths";
 
 const { auth } = NextAuth(authOptions);
-const privateRoutes: string[] = Object.values(APP_PATHS.dashboard).map(String);
-const authRoutes: string[] = Object.values(APP_PATHS.auth).map(String);
+const privateRoutes: string[] = Object.values(APP_PATHS.dashboard).map(String); // Private routes
+const authRoutes: string[] = Object.values(APP_PATHS.auth).map(String); // Auth routes
+// Middleware to protect private routes
 export default auth(async function middleware(req) {
   const isLoggedIn = !!req.auth;
   const { nextUrl } = req;
-  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname); // Check if the current URL is a private route
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname); // Check if the current URL is a auth route
+  /* Check if the user is logged in and is trying to access a private route, or if the user is not logged in and is trying to access an auth route. If either condition is true, redirect the
+     user to the login page.
+     */
   if (isPrivateRoute && (!isLoggedIn || isAuthRoute)) {
     return Response.redirect(new URL(APP_PATHS.auth.login, nextUrl));
   }
-
+  /* Check if the user is logged in and is trying to access a private route and is not logged in. 
+  If either condition is true, redirect the user to the dashboard.
+  */
   if (isPrivateRoute && isLoggedIn) {
     return Response.redirect(new URL(APP_PATHS.dashboard.root, nextUrl));
   }
-  console.log(nextUrl.pathname);
+  /*
+  If the user is not logged in and is trying to access an auth route, redirect the user to the dashboard.
+  */
+  if (isAuthRoute && !isLoggedIn) {
+    return Response.redirect(new URL(APP_PATHS.dashboard.root, nextUrl));
+  }
+  /* If the user is not logged in and is trying to access a private route, redirect the user to the login page. */
   if (isPrivateRoute && !isLoggedIn) {
     return Response.redirect(new URL(APP_PATHS.auth.login, nextUrl));
   }
-
+  /* If the user is logged in and is trying to access an auth route, redirect the user to the dashboard. */
   if (isAuthRoute && !isLoggedIn) return;
 });
 
