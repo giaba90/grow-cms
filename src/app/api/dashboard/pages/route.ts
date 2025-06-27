90c1e6cb4acbd8882b266ea0e8274d7faad7e48e
Gianluca Barranca <info@gianlucabarranca.it>
2025-06-27 21:36:13 +0200

    feat: update URL handling in POST request to ensure unique URLs by appending a random number for existing titles

--- src/app/api/dashboard/pages/route.ts
+++ src/app/api/dashboard/pages/route.ts
@@ -37,13 +37,15 @@ export async function POST(req: Request) {
     const { title, content, status, url } = data;
     // check if pages exist into database
     const existingPage = await prisma.page.findFirst({
-      where: { url },
+      where: { title },
     });
 
     let newUrl: string = "";
+
     if (existingPage) {
       // If a page with the same URL already exists add number to the URL
-      newUrl = `${url}-${existingPage.id}`;
+      const randomNum = Math.floor(Math.random() * 10000);
+      newUrl = `${slugify(title, { lower: true, strict: true })}-${randomNum}`;
     } else {
       newUrl = url || slugify(title, { lower: true, strict: true })
     }
